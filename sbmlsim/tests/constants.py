e9bb0d540bf6829f424bf7e39e37f8a76969a1c4
Matthias Koenig <konigmatt@googlemail.com>
2019-12-05 14:33:53 +0100

    example midazolam

--- sbmlsim/tests/constants.py
+++ sbmlsim/tests/constants.py
@@ -10,3 +10,6 @@ DATA_PATH = TEST_PATH / 'data'  # directory of data for tests
 MODEL_REPRESSILATOR = DATA_PATH / "models" / "repressilator.xml"
 MODEL_GLCWB = DATA_PATH / "models" / "body21_livertoy_flat.xml"
 MODEL_DEMO = DATA_PATH / "models" / "Koenig_demo_14.xml"
+
+# FIXME: make unitreg work with legacy paths
+MODEL_MIDAZOLAM = Path("/home/mkoenig/git/sbmlutils/sbmlutils/examples/models/midazolam/midazolam_model.xml")
