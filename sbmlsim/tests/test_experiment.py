58e03926aba4ffd2ffe599db3bb55c5de9a2699a
Matthias Koenig <konigmatt@googlemail.com>
2020-04-07 16:42:46 +0200

    cleanup for test coverage

--- sbmlsim/tests/test_experiment.py
+++ sbmlsim/tests/test_experiment.py
@@ -1,5 +1,10 @@
 from sbmlsim.examples.experiments.repressilator import repressilator
+from sbmlsim.examples.experiments.glucose import glucose
 
 
 def test_repressilator_experiment(tmp_path):
     repressilator.run(output_path=tmp_path)
+
+
+def test_glucose_experiment(tmp_path):
+    glucose.glucose_experiment()
