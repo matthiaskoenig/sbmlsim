46e20f476ef296ec0175af793cebee91a6f90e9e
Matthias Koenig <konigmatt@googlemail.com>
2020-04-07 16:37:24 +0200

    cleanup for model coverage

--- sbmlsim/oven/fit/fit_example.py
+++ sbmlsim/oven/fit/fit_example.py
@@ -27,21 +27,20 @@ def residual(p, x, y):
 
     return y - f(x, *p)
 
-
-if __name__ == "__main__":
+def fit_example():
     # create trainings data
     x = np.linspace(0, 20, 20)
-    y = f(x, a=1.5, b=5, c=0.2) + 20*np.random.normal(size=len(x))
+    y = f(x, a=1.5, b=5, c=0.2) + 20 * np.random.normal(size=len(x))
 
     plt.plot(x, y, 'or')
 
     for k in range(20):
-
         p0 = [1., 1., 1.] + 2.0 * np.random.normal(size=3)
         p0 = [1.6, 6., 0.1] + 2.0 * np.random.normal(size=3)
 
         # popt, pcov = optimize.leastsq(residual, p0, args=(x, y))
-        popt, pcov = optimize.least_squares(fun=residual, x0=p0, bounds=(-np.inf, np.inf),
+        popt, pcov = optimize.least_squares(fun=residual, x0=p0,
+                                            bounds=(-np.inf, np.inf),
                                             args=(x, y))
 
         print(p0, "->", popt)
@@ -51,3 +50,7 @@ if __name__ == "__main__":
         yn = f(xn, *popt)
         plt.plot(xn, yn, color="black", alpha=0.8)
     plt.show()
+
+
+if __name__ == "__main__":
+    fit_example()
