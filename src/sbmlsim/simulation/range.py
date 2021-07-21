04de5569bc642d672f9b97f0090e89f64daafc2b
Matthias Koenig <konigmatt@googlemail.com>
2021-07-21 23:43:26 +0200

    work on range

--- src/sbmlsim/simulation/range.py
+++ src/sbmlsim/simulation/range.py
@@ -1,3 +1,4 @@
+from enum import Enum, unique, auto
 import itertools
 from typing import Dict, Iterable, List, Union, Tuple
 
@@ -30,10 +31,23 @@ class Range(BaseObject):
     def __init__(self, sid: str, name: str = None):
         """Construct Range."""
         super(Range, self).__init__(sid=sid, name=name)
+        self._values: np.ndarray
 
     def __repr__(self) -> str:
         """Get string representation."""
         return f"Range({self.sid}, {self.name}"
+
+    @property
+    def values(self) -> np.ndarray:
+        """Get values of the range."""
+        return self._values
+
+    @values.setter
+    def values(self, data: np.ndarray):
+        """Set values for range."""
+        if not isinstance(data, np.ndarray):
+            raise ValueError(f"'data' in Range must be numpy.ndarray, but '{type(data)}' for "
+                        f"'{data}'")
 
 
 class VectorRange(Range):
@@ -70,58 +84,115 @@ class VectorRange(Range):
         return f"Range(sid={self.sid}, name={self.name})"
 
 
+class UniformRangeType(Enum):
+    """UniformRangeType.
+
+    Attribute type that can take the values linear or log.
+    Determines whether to draw the values logarithmically (with a base of 10) or linearly.
+    """
+
+    linear = auto()
+    log = auto()
+
+
 class UniformRange(Range):
     """UniformRange class.
 
-    TODO:
+    The UniformRange on the preceding page) allows the definition of a Range with uniformly spaced values.
+    The range_type determines whether to draw the values logarithmically (with a base of 10) or linearly.
+
     """
 
-    def __init__(self, sid: str, values: Union[List, Tuple, np.ndarray], name: str = None,):
+    def __init__(self, sid: str, start: float, end: float, steps: int, range_type: UniformRangeType, name: str = None):
         """Construct VectorRange."""
         super(UniformRange, self).__init__(sid=sid, name=name)
-        if isinstance(values, (list, tuple)):
-            values = np.array(values)
+        self.start: float = start
+        self.end: float = end
+        self.steps: int = steps
+        self.range_type: UniformRangeType = range_type
 
-        if not isinstance(values, np.ndarray):
-            raise ValueError(f"'values' in VectorRange must be numpy.ndarray, but '{type(values)}' for "
-                             f"'{values}'")
-        if values.ndim != 1:
-            raise ValueError(
-                f"'values' in VectorRange must be one-dimensional, but ndim='{values.ndim}' for "
-                f"'{values}'")
-
-        # values are sorted
-        values_sorted = np.sort(values)
-        if not np.allclose(values, values_sorted):
-            console.log(
-                f"'values' in VectorRange must be one-dimensional, but ndim='{values.ndim}' for "
-                f"'{values}'")
-        self.values: np.ndarray = values.sort()
+        if self.range_type == UniformRangeType.linear:
+            self.values = np.linspace(start=start, end=end, num=steps+1)
+        elif self.range_type == UniformRangeType.log:
+            # In linear space, the sequence starts at ``base ** start``
+            # (`base` to the power of `start`) and ends with ``base ** stop
+            self.values = np.logspace(
+                start=np.log10(self.start), 
+                end=np.log10(self.end), 
+                num=self.steps+1, 
+                base=10
+            )
+        else:
+            raise ValueError(f"Unsupported range type in UniformRange: '{self.range_type}'")
 
     def __repr__(self) -> str:
         """Get string representation."""
-        return f"Range(sid={self.sid}, name={self.name})"
+        return f"UniformRange(sid={self.sid}, name={self.name}, start={self.start}, stop={self.stop}, steps={self.steps})"
+
+
+class DataRange(Range):
+    """DataRange class.
+
+    The DataRange constructs a range by reference to external data. 
+    The sourceRef must point to a DataDescription with a single dimension, 
+    whose values are used as the values of the range.
+    """
+
+    def __init__(self, sid: str, source_ref: str, name: str = None):
+        """Construct DataRange."""
+        super(DataRange, self).__init__(sid=sid, name=name)
+        self.source_ref: str = source_ref
+
+    def __repr__(self) -> str:
+        """Get string representation."""
+        return f"DataRange(sid={self.sid}, name={self.name}, source_ref={self.source_ref})"
+
+    @property
+    def values(self) -> np.ndarray:
+        """Resolve data from data generator."""
+
+        #FIXME: implement; requires access to the resolved DataDescriptions of the experiment.
+        raise NotImplementedError
 
 
 
 
-class UniformRange(Range):
-    start: float
-    end: float
-    steps: float
-    type: str  # linear/log
+
+class FunctionalRange(Range):
+    """FunctionalRange class.
+
+    The FunctionalRange constructs a range through calculations that
+    determine the next value based on the value(s) of other range(s) or model variables. 
+    In this it is similar to the ComputeChange element, and shares some of the same 
+    child elements (but is not a subclass of ComputeChange).
+    """
+
+    def __init__(self, sid: str, range: str, name: str = None):
+        """Construct DataRange."""
+        super(DataRange, self).__init__(sid=sid, name=name)
+        self.source_ref: str = source_ref
+
+    def __repr__(self) -> str:
+        """Get string representation."""
+        return f"DataRange(sid={self.sid}, name={self.name}, source_ref={self.source_ref})"
+
+    @property
+    def values(self) -> np.ndarray:
+        """Resolve data from data generator."""
+
+        #FIXME: implement; requires access to the resolved DataDescriptions of the experiment.
+        raise NotImplementedError
 
 
-# @dataclass
-# class DataRange(Range):
-#     source: str
-#
 # @dataclass
 # class FunctionalRange(Range):
 #     range: Range
 #     variables: List   # model variables
 #     parameters: List
 #     math: str
+
+
+
 
 
 class Dimension:
