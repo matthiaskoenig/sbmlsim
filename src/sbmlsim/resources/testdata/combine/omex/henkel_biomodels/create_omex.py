13615f4463389f2005a6eabb72b90cc3f453d181
Matthias Koenig <konigmatt@googlemail.com>
2022-02-20 17:37:08 +0100

    removed libcombine from entry

---
+++ src/sbmlsim/resources/testdata/combine/omex/henkel_biomodels/create_omex.py
@@ -0,0 +1,48 @@
+"""
+Creates the omex files from the SED-ML files.
+"""
+
+from pathlib import Path
+from typing import List
+
+from pymetadata import omex as pyomex
+
+from sbmlutils import log
+logger = log.get_logger(__name__)
+
+
+EXAMPLES_DIR: Path = Path(__file__).parent
+SEDML_DIR: Path = EXAMPLES_DIR / "sedml"
+OMEX_DIR: Path = EXAMPLES_DIR / "omex"
+
+
+def create_omex_from_sedml(sedml_path: Path, omex_path: Path) -> None:
+    """Creates a combine archive from SED-ML path."""
+
+    omex = pyomex.Omex()
+    entry = pyomex.ManifestEntry(
+        master=True,
+        format=pyomex.EntryFormat.SEDML,
+        location=f"./{sedml_path.name}"
+    )
+    omex.add_entry(entry_path=sedml_path, entry=entry)
+    omex.to_omex(omex_path=omex_path)
+    logger.info(omex)
+
+
+def create_all_omex() -> None:
+    """Creates all omex from the SED-ML file."""
+
+    sedml_paths: List[Path] = []
+    for p in SEDML_DIR.rglob("*"):
+        sedml_suffixes = {'.xml', '.sedml'}
+        if p.is_file() and p.suffix in sedml_suffixes:
+            sedml_paths.append(SEDML_DIR / p)
+
+    for p in sorted(sedml_paths):
+        logger.info(f"{p.name}")
+        create_omex_from_sedml(sedml_path=p, omex_path=OMEX_DIR / f"{p.stem}.omex")
+
+
+if __name__ == "__main__":
+    create_all_omex()
